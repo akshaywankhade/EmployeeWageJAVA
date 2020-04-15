aea3c8c9838c41e315da7f0156031510cd98dcd0
Akshay Wankhade <akshuwankhade12@gmail.com>
2020-04-15 17:14:50 +0530

    ADDED: Calculate Part Time Employee wage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,24 +1,25 @@
-public class EmpWageBuilder {
+public class EmpWageBuilder{
+	//CONSTANTS 
+	public static final int EMPLOYEE_WAGE_PER_HOUR=20;
+	public static final int IS_PART_TIME=1;
+	public static final int IS_FULL_TIME=2;
 
-	public static void main(String[] args) {
-	//Constants
-	int IS_FULL_TIME = 1;
-	int EMP_RATE_PER_HOUR = 20;
-	//Variable
-	int empHrs = 0;
-	int empWage = 0;
-	//Computation
-	double empCheck = Math.floor(Math.random() * 10) % 2;
-	if (empCheck == IS_FULL_TIME) {
-		System.out.println("Employee is Present");
-		empHrs = 8;
-	}
-	else {
-		System.out.println("Employee is Absent");
-		empHrs = 0;
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage :" + empWage);
-	}
-	}
+	public static void main (String[] args){
+		//VARIABLES
+		int empHrs=0;
+		int empWage=0;
+
+		//COMPUTATION
+		double empCheck=Math.floor(Math.random() *10)% 3;
+		if ( empCheck == IS_FULL_TIME ){
+			empHrs=8;
+		}
+		else if(empCheck == IS_PART_TIME){
+			empHrs=4;
+		}
+		else
+			empHrs=0;
+		empWage=empHrs * EMPLOYEE_WAGE_PER_HOUR;
+		System.out.println("Employee Wage: "+empWage);
+		}
 }
-
